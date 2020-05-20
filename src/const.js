52fb40e0156e613bae80b5c68f06f34cbdfe9910
Kildare Lauser <kildarealauser@gmail.com>
2020-05-20 08:52:55 -0400

    server api url

--- src/const.js
+++ src/const.js
@@ -1,2 +1,2 @@
-//export const API = "http://10.10.0.250/DolaresLeonux/api/";
-export const API = "http://localhost:2114/api/";
+export const API = "http://10.10.0.250/DolaresLeonux/api/";
+//export const API = "http://localhost:2114/api/";
