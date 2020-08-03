cbe7dd26e94fbd7a75fa35043108d362d714db17
Kildare Lauser <kildarealauser@gmail.com>
2020-08-03 07:52:46 -0400

    Add to pickup and delivery process

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,12 +8,22 @@ class Home extends Component {
     super();
     this.state = {
       articles: [],
+      client: {
+        id: "",
+        name: "",
+        birthday: "",
+        location: "",
+        email: "",
+        phone: "",
+        created: true,
+      },
       totalBs: 0,
       totalDolares: 0,
       account: "",
       isLoading: false,
       buttonsEnable: false,
       tasa: 0,
+      custom: false,
     };
   }
 
@@ -24,6 +34,7 @@ class Home extends Component {
       {
         ...this.state,
         isLoading: true,
+        articles: [],
       },
       () => {
         fetch(`${API}article/${this.state.account}/?db=${DB}`)
@@ -88,23 +99,32 @@ class Home extends Component {
               this.setState(
                 {
                   articles: [],
+                  client: {
+                    id: "",
+                    name: "",
+                    birthday: "",
+                    location: "",
+                    email: "",
+                    phone: "",
+                    created: true,
+                  },
                   totalBs: 0,
                   totalDolares: 0,
                   account: "",
                   isLoading: false,
+                  buttonsEnable: false,
+                  tasa: 0,
+                  custom: false,
                 },
                 () => alert("Procesado")
               );
             } else {
               this.setState(
                 {
-                  articles: [],
-                  totalBs: 0,
-                  totalDolares: 0,
-                  account: "",
+                  ...this.state,
                   isLoading: false,
                 },
-                () => alert("Procesado")
+                () => alert(res.description)
               );
             }
           });
@@ -125,11 +145,206 @@ class Home extends Component {
     }
   };
 
+  handleSubmitDelivery = (e) => {
+    e.preventDefault();
+    if (this.props.currentUser.Administrator) {
+      this.handleProcess("2");
+    }
+  };
+
+  handleSubmitPickUp = (e) => {
+    e.preventDefault();
+    if (this.props.currentUser.Administrator) {
+      this.handleProcess("3");
+    }
+  };
+
   handleChange = (e) => {
     this.setState({
       ...this.state,
       account: e.target.value,
     });
+  };
+
+  handleClientChange = (e) => {
+    //cuando se modifica un input
+    const { value, name } = e.target;
+    if (name === "id") {
+      const size = this.state.client.id.length;
+      const types = ["V", "E", "J", "G"];
+      if (size === 0) {
+        if (types.indexOf(value.toUpperCase()) == -1) {
+          alert("Debe de ser un tipo valido (V|E|J|G)");
+        }
+        this.setState({
+          ...this.state,
+          client: {
+            ...this.state.client,
+            [name]: value.toUpperCase(),
+          },
+        });
+      } else {
+        this.setState({
+          ...this.state,
+          client: {
+            ...this.state.client,
+            [name]: value,
+          },
+        });
+      }
+    } else {
+      this.setState({
+        ...this.state,
+        client: {
+          ...this.state.client,
+          [name]: value,
+        },
+      });
+    }
+  };
+
+  handleCustom = (e) => {
+    this.setState({
+      ...this.state,
+      custom: !this.state.custom,
+      client: {
+        id: "",
+        name: "",
+        birthday: "",
+        email: "",
+        phone: "",
+        created: true,
+      },
+    });
+  };
+
+  handleClientSearch = (e) => {
+    e.preventDefault();
+    const { DB } = this.props.currentUser;
+    this.setState(
+      {
+        ...this.state,
+        isLoading: true,
+      },
+      () => {
+        fetch(`${API}client/${this.state.client.id}/?db=${DB}`)
+          .then((res) => res.json())
+          .then((res) => {
+            if (res.error === false) {
+              this.setState({
+                ...this.state,
+                isLoading: false,
+                client: {
+                  id: res.data[0].IdentificationNumber,
+                  name: res.data[0].Name,
+                  birthday: res.data[0].Birthday,
+                  email: res.data[0].Email,
+                  phone: res.data[0].Phone,
+                  created: true,
+                },
+              });
+            } else {
+              if (res.description === "client doesn't exists!") {
+                if (
+                  window.confirm(
+                    "El cliente con la identificación indicada no existe\n¿Deseas crear el cliente?"
+                  )
+                ) {
+                  this.setState({
+                    ...this.state,
+                    client: {
+                      ...this.state.client,
+                      name: "",
+                      birthday: "",
+                      location: "",
+                      email: "",
+                      phone: "",
+                      created: false,
+                    },
+                    isLoading: false,
+                  });
+                } else {
+                  this.setState({
+                    ...this.state,
+                    isLoading: false,
+                  });
+                }
+              } else {
+                this.setState(
+                  {
+                    ...this.state,
+                    isLoading: false,
+                  },
+                  () => {
+                    alert(res.description);
+                  }
+                );
+              }
+            }
+          });
+      }
+    );
+  };
+
+  handleCreateClient = (e) => {
+    e.preventDefault();
+    const { DB } = this.props.currentUser;
+    const { id, name, birthday, location, phone, email } = this.state.client;
+    this.setState(
+      {
+        ...this.state,
+        isLoading: true,
+      },
+      () => {
+        if (id !== "" && name !== "") {
+          // post login to server
+          fetch(`${API}client/?db=${DB}`, {
+            method: "post",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              Code: id,
+              IdentificationNumber: id,
+              Name: name,
+              Direccion: location,
+              Phone: phone,
+              Email: email,
+              Birthday: birthday,
+            }),
+          })
+            .then((res) => res.json())
+            .then((res) => {
+              if (!res.error) {
+                if (res.description === "Client created successfully") {
+                  this.setState({
+                    ...this.state,
+                    isLoading: false,
+                    client: {
+                      ...this.state.client,
+                      created: true,
+                    },
+                  });
+                }
+              } else {
+                this.setState(
+                  {
+                    ...this.state,
+                    isLoading: false,
+                  },
+                  () => {
+                    alert("Error creando el cliente!\n" + res.description);
+                  }
+                );
+              }
+            });
+        } else {
+          this.setState({ ...this.state, isLoading: false }, () => {
+            alert("Verificar datos faltantes!");
+          });
+        }
+      }
+    );
   };
 
   render() {
@@ -192,16 +407,190 @@ class Home extends Component {
           </div>
 
           <div className="row">
+            <div className="row">
+              <a
+                className="waves-effect waves-light orange btn col s4 m2"
+                onClick={this.handleCustom}
+                href="#"
+                style={{ marginLeft: 0 }}
+                disabled={this.state.buttonsEnable ? false : true}
+              >
+                {this.state.custom ? "No Personalizada" : "Personalizada"}
+              </a>
+            </div>
+            <div className={`row ${this.state.custom ? null : "hide"}`}>
+              <div className="row valign-wrapper">
+                <div
+                  className="input-field col s8 m5"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="text"
+                    name="id"
+                    id="id"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.id}
+                  />
+                  <label
+                    htmlFor="id"
+                    className={this.state.client.id !== "" ? "active" : ""}
+                  >
+                    Ingresa Doc del cliente (Ej: V26392347)
+                  </label>
+                </div>
+                <a
+                  className="waves-effect waves-light orange btn col s4 m2"
+                  onClick={this.handleClientSearch}
+                  href="#"
+                  style={{ marginLeft: 0 }}
+                >
+                  BUSCAR
+                </a>
+              </div>
+              <div className="row">
+                <div
+                  className="input-field col s8 m3"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.name}
+                    disabled={this.state.client.created ? true : false}
+                  />
+                  <label
+                    htmlFor="name"
+                    className={this.state.client.name !== "" ? "active" : ""}
+                  >
+                    Nombre y Apellido del Cliente
+                  </label>
+                </div>
+                <div
+                  className="input-field col s8 m2"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="text"
+                    name="birthday"
+                    id="birthday"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.birthday}
+                    disabled={this.state.client.created ? true : false}
+                  />
+                  <label
+                    htmlFor="birthday"
+                    className={
+                      this.state.client.birthday !== "" ? "active" : ""
+                    }
+                  >
+                    Fecha de nacimiento (dd/mm/yyyy)
+                  </label>
+                </div>
+                <div
+                  className="input-field col s8 m3"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.email}
+                    disabled={this.state.client.created ? true : false}
+                  />
+                  <label
+                    htmlFor="email"
+                    className={this.state.client.email !== "" ? "active" : ""}
+                  >
+                    Correo
+                  </label>
+                </div>
+                <div
+                  className="input-field col s8 m3"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="tel"
+                    name="phone"
+                    id="phone"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.phone}
+                    disabled={this.state.client.created ? true : false}
+                  />
+                  <label
+                    htmlFor="phone"
+                    className={this.state.client.phone !== "" ? "active" : ""}
+                  >
+                    Telefono
+                  </label>
+                </div>
+                <div
+                  className="input-field col s12 m6"
+                  style={{ marginLeft: 0 }}
+                >
+                  <input
+                    type="text"
+                    name="location"
+                    id="location"
+                    onChange={this.handleClientChange}
+                    value={this.state.client.location}
+                    disabled={this.state.client.created ? true : false}
+                  />
+                  <label
+                    htmlFor="location"
+                    className={
+                      this.state.client.location !== "" ? "active" : ""
+                    }
+                  >
+                    Dirección
+                  </label>
+                </div>
+                <a
+                  className={`waves-effect waves-light orange btn col s12 m6 ${
+                    this.state.client.created ? "hide" : null
+                  }`}
+                  onClick={this.handleCreateClient}
+                  href="#"
+                  style={{ marginLeft: 0 }}
+                  disabled={this.state.client.created ? true : false}
+                >
+                  CREAR CLIENTE
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
             <a
-              className="waves-effect waves-light orange btn col s5"
+              className="waves-effect waves-light orange btn col s3"
               onClick={this.handleSubmit}
               disabled={this.state.buttonsEnable ? false : true}
             >
-              PROCESAR
+              PAGO DIVISAS
             </a>
             {this.props.currentUser.Administrator ? (
               <a
-                className="waves-effect waves-light orange btn col s5 right"
+                className="waves-effect waves-light orange btn col s3"
+                onClick={this.handleSubmitDelivery}
+                disabled={this.state.buttonsEnable ? false : true}
+              >
+                DELIVERY
+              </a>
+            ) : null}
+            {this.props.currentUser.Administrator ? (
+              <a
+                className="waves-effect waves-light orange btn col s3"
+                onClick={this.handleSubmitPickUp}
+                disabled={this.state.buttonsEnable ? false : true}
+              >
+                TO PICK-UP
+              </a>
+            ) : null}
+            {this.props.currentUser.Administrator ? (
+              <a
+                className="waves-effect waves-light orange btn col s3 right"
                 onClick={this.handleSubmitWithoutPay}
                 disabled={this.state.buttonsEnable ? false : true}
               >
